2ac0c4c9e30c109bf4c888773326653f584ae190
Josiah Maina <josiah5115@gmail.com>
2018-09-12 22:09:14 -0600

    payload obj

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = reqiuire('axios');
+const axios = require('axios');
 const session = require('express-session');
 
 
@@ -12,10 +11,10 @@ const app = express();
 
 // process.env destructures:
 let {
-  REACT_APP_CLIENT_ID,
-  CLIENT_SECRET,
+  REACT_APP_AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET,
   SERVER_PORT,
-  SECRET
+  AUTH0_API_CLIENT_SECRET
 } = process.env;
 
 
@@ -23,48 +22,65 @@ let {
 app.use(bodyParser.json());
 
 app.use(session({
-  secret: SECRET,
+  secret: AUTH0_API_CLIENT_SECRET,
   resave: false,
   saveUninitialized: true
 }));
 
 
-
 // endpoints:
 app.get('/callback', (req, res) => {
-    exchangeCodeForAccessToken()
-      .then(exchangeAccessTokenForUserInfo)
-      .then(fetchAuth0AccessToken)
-      .then(fetchGitHubAccessToken)
-      // .then(setGitTokenToSession)
-      .catch(error => {
-          console.log(error, 'Server errror');
-          res.status(500).send('An error occured on the server. Check the terminal');
-      });
+  console.log(callback, 'The callback is getting hit!')
+  exchangeCodeForAccessToken()
+    .then(exchangeAccessTokenForUserInfo)
+    .then(fetchAuth0AccessToken)
+    .then(fetchGitHubAccessToken)
+    .catch(error => {
+      console.log('Internal Server Error', error);
+      res.status(500).send('An error occurred on the server. Check the terminal.');
+    });
 
     function exchangeCodeForAccessToken() {
-        const payload = {
-            client_id: REACT_APP_CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            code: req.query.code,
-            grant_type: 'authorization_code',
-            redirect_uri: `http://${req.headers.host}/auth/callback`
-          };
+      const payload = {
+        client_id: REACT_APP_AUTH0_CLIENT_ID,
+        client_secret: AUTH0_CLIENT_SECRET,
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: `http://${req.headers.host}/auth/callback`
+      };
 
-          return axios.post(`https://${REACT_APP_AUTH0_DOMAIN}/oauth/`)
-    }  
-},
+      return axios.post(`https://${REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload);
+    };
 
-app.get('/api/user-data', (req, res) => {
-  res.status(200).json(req.session.user)
-}),
+    function exchangeAccessTokenForUserInfo(accessTokenResponse) {
+      const accessToken = accessTokenResponse.data.access_token;
+      return axios.get(`https://${REACT_APP_AUTH0_DOMAIN}/userinfo?access_token=${accessToken}`);
+    };
 
-app.post('/api/logout', (req, res) => {
-  req.session.destroy();
-  res.send('logged out');
-})
-,
+    function fetchAuth0AccessToken(userInfoResponse) {
+      req.session.user = userInfoResponse.data;
 
-// Server port listening:
-app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}`); });
+      const payload = {
+        grant_type: 'authorization_code',
+        client_id: AUTH0_API_CLIENT_ID,
+        client_secret: AUTH0_API_CLIENT_SECRET,
+        audience: `https://{REACT_APP_AUTH0_DOMAIN}/api/v2`
+      };
+
+      return axios.post(`https://${REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload);
+    }
+  },
+
+  app.get('/api/user-data', (req, res) => {
+    res.status(200).json(req.session.user)
+  }),
+
+  app.post('/api/logout', (req, res) => {
+    req.session.destroy();
+    res.send('logged out');
+  }),
+
+  // Server port listening:
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
+  }));
